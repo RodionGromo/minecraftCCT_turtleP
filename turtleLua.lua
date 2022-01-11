062e573b48643048052cd5b58f133a544e91632c
TheGrom <49732296+RodionGromo@users.noreply.github.com>
2022-01-12 01:13:13 +0300

    major updates
    some bugs but I'll work on it

--- turtleLua.lua
+++ turtleLua.lua
@@ -1,7 +1,12 @@
 json = require "json"
 print("opening websocket")
 ws = http.websocket("ws://localhost:9000")
-ws.send(json.encode({message="socketInfo",who="robot"}))
+if(os.getComputerLabel() ~= nil ) then
+	ws.send(json.encode({message="socketInfo",who="robot",id=os.getComputerLabel()}))
+else
+	ws.send(json.encode({message="socketInfo",who="robot"}))
+end
+
 while true do
 	message = ws.receive()
 	print("Got: " .. message)
@@ -9,16 +14,18 @@ while true do
 	status,value,value2 = pcall(funct)
 	if(status) then
 		if(value2 ~= nil) then
-			string1 = json.encode({message="returnInfo",val=value2,id=os.getComputerLabel()})
-		elseif(value == nil) then
-			string1 = json.encode({message="returnBool",val=status,id=os.getComputerLabel()})
+			string1 = json.encode({message="returnInfo",val=value2,id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
+		elseif(type(value) == "table") then
+			string1 = json.encode({message="returnInfo",val=value,id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
+		elseif(type(value) == "boolean") then
+			string1 = json.encode({message="returnBool",val=value,id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
 		else
-			string1 = json.encode({message="returnValue",val=tostring(value),id=os.getComputerLabel()})
+			string1 = json.encode({message="returnValue",val=tostring(value),id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
 		end
 		print("good command, sending: " .. string1)
 		ws.send(string1)
 	else
 		print("bad command")
-		ws.send(json.encode({message="err",id=getComputerLabel()}))
+		ws.send(json.encode({message="err",id=os.getComputerLabel(),fuel=turtle.getFuelLevel()}))
 	end
 end
