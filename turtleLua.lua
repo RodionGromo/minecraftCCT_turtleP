08930a68d3614a9c7550a2bc4767d6cee1b59f63
TheGrom <49732296+RodionGromo@users.noreply.github.com>
2023-07-14 21:09:07 +0300

    Update turtleLua.lua
    big update

--- turtleLua.lua
+++ turtleLua.lua
@@ -1,31 +1,27 @@
 json = require "json"
 print("opening websocket")
-ws = http.websocket("ws://localhost:9000")
-if(os.getComputerLabel() ~= nil ) then
+ws, status = http.websocket("ws://localhost:9000")
+
+if(os.getComputerLabel() ~= nil) then
 	ws.send(json.encode({message="socketInfo",who="robot",id=os.getComputerLabel()}))
 else
 	ws.send(json.encode({message="socketInfo",who="robot"}))
 end
 
 while true do
-	message = ws.receive()
-	print("Got: " .. message)
-	funct = load(message)
-	status,value,value2 = pcall(funct)
-	if(status) then
-		if(value2 ~= nil) then
-			string1 = json.encode({message="returnInfo",val=value2,id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
-		elseif(type(value) == "table") then
-			string1 = json.encode({message="returnInfo",val=value,id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
-		elseif(type(value) == "boolean") then
-			string1 = json.encode({message="returnBool",val=value,id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
-		else
-			string1 = json.encode({message="returnValue",val=tostring(value),id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
-		end
-		print("good command, sending: " .. string1)
-		ws.send(string1)
+	recv_data = ws.receive()
+	if(recv_data == nil) then
+		print("websocket was closed or timeout")
+		break
 	else
-		print("bad command")
-		ws.send(json.encode({message="err",id=os.getComputerLabel(),fuel=turtle.getFuelLevel()}))
+		print("Got: " .. recv_data)
+		funct = load("return ".. recv_data)
+		status,value,value2 = pcall(funct)
+		if(status) then
+			message = json.encode({message="returnData",val1=value,val2=value2,id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
+		else
+			message = json.encode({message="commandError",id=os.getComputerLabel(),fuel=turtle.getFuelLevel()})
+		end
+		ws.send(message)
 	end
 end
